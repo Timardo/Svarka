20a9a3e704751b6a5a7392570de88d435669bf7a
santagithub <exiscents@gmail.com>
2018-01-29 17:48:49 +0300

    Fix

--- src/main/java/org/bukkit/craftbukkit/util/ServerShutdownThread.java
+++ src/main/java/org/bukkit/craftbukkit/util/ServerShutdownThread.java
@@ -4,6 +4,7 @@
 
 package org.bukkit.craftbukkit.util;
 
+import net.minecraft.server.ExceptionWorldConflict;
 import net.minecraft.server.MinecraftServer;
 
 public class ServerShutdownThread extends Thread
